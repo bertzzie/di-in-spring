9dd071f382732fcc3331a4fc5445cc120817ead3
Alex Xandra Albert Sim <bertzzie@gmail.com>
2020-04-18 18:14:37 +0700

    Initial commit

---
+++ src/main/java/tech/namas/demo/ioc/DependencyInjectionApplication.java
@@ -0,0 +1,9 @@
+package tech.namas.demo.ioc;
+
+public class DependencyInjectionApplication {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/test/java/tech/namas/demo/ioc/DependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.namas.demo.ioc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInjectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
