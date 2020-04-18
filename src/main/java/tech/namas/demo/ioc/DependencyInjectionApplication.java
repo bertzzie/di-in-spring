dfea9a029c4e17e32a6d7f32a840c6e174654d86
Alex Xandra Albert Sim <bertzzie@gmail.com>
2020-04-18 20:37:35 +0700

    Lesson 2: Application Context

--- src/main/java/tech/namas/demo/ioc/DependencyInjectionApplication.java
+++ src/main/java/tech/namas/demo/ioc/DependencyInjectionApplication.java
@@ -1,6 +1,7 @@
 package tech.namas.demo.ioc;
 
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import tech.namas.demo.ioc.configs.ServiceConfiguration;
 import tech.namas.demo.ioc.services.SpeakService;
@@ -8,9 +9,11 @@ import tech.namas.demo.ioc.services.SpeakService;
 public class DependencyInjectionApplication {
 
     public static void main(String[] args) {
-        BeanFactory beanFactory = new AnnotationConfigApplicationContext(ServiceConfiguration.class);
-        SpeakService speakService = beanFactory.getBean(SpeakService.class);
+        ApplicationContext context =
+            new AnnotationConfigApplicationContext(ServiceConfiguration.class);
+        BeanFactory beanFactory = context.getAutowireCapableBeanFactory();
 
+        SpeakService speakService = beanFactory.getBean(SpeakService.class);
         speakService.sayHello("world");
     }
 
